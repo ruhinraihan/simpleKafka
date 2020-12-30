1d30743f1e8614cac0d4c87dcf0681a193058f0d
Raihan Ruhin <dev.raihanruhin@gmail.com>
2020-12-30 20:04:00 +0900

    simplest kafka app

---
+++ src/main/java/com/simpleexample/kafka_producer_consumer/KafkaSimpleController.java
@@ -0,0 +1,32 @@
+package com.simpleexample.kafka_producer_consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/kafka")
+public class KafkaSimpleController {
+// producer
+    private KafkaTemplate<String, SimpleModel> kafkaTemplate;
+
+    @Autowired
+    public KafkaSimpleController(KafkaTemplate<String, SimpleModel> kafkaTemplate){
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping
+    public void post(@RequestBody SimpleModel simpleModel){
+        kafkaTemplate.send("myTopic", simpleModel);
+    }
+
+// consumer
+    @KafkaListener(topics = "myTopic")
+    public void getFromKafka(SimpleModel simpleModel){
+        System.out.println(simpleModel.getField1());
+    }
+}
